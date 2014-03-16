a73be987fd52d3741ca44a6b6dd0af72de14515a
Rahul <rahul.iragavarapu@gmail.com>
2014-03-16 21:00:08 +0530

    first commit

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Principal;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class wifire : Form
+    {
+        string suc,wion; 
+        public wifire()
+        {
+            InitializeComponent();
+            suc="The hosted network mode has been set to allow.";
+            wion = "The hosted network started.";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                new System.Diagnostics.ProcessStartInfo("cmd", "/c " + "netsh wlan start hostednetwork");
+
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                System.Diagnostics.Debug.WriteLine(result);
+                if (result.Contains(wion))
+                    label3.Text = result;
+                else
+                    label3.Text = "Please switch on wifi";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.ProcessStartInfo procStartInfo =
+            new System.Diagnostics.ProcessStartInfo("cmd", "/c " + "netsh wlan set hostednetwork mode=allow ssid="+textBox1.Text+" key="+textBox2.Text);
+
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            string result = proc.StandardOutput.ReadToEnd();
+            if(result.Contains(suc))
+            label3.Text = "successfuly created";
+            else
+            label3.Text = "password must be between 8-63";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.ProcessStartInfo procStartInfo =
+            new System.Diagnostics.ProcessStartInfo("cmd", "/c " + "netsh wlan stop hostednetwork");
+
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            string result = proc.StandardOutput.ReadToEnd();
+            System.Diagnostics.Debug.WriteLine(result);
+            label3.Text = result;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("created by Lucypher\r\nand Sai krishna","about");
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        static void Main()
+        {
+            WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool hasAdministrativeRight = pricipal.IsInRole(WindowsBuiltInRole.Administrator);
+            if(hasAdministrativeRight)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new wifire());
+            }
+            else if (!hasAdministrativeRight)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.UseShellExecute = true;
+                startInfo.WorkingDirectory = Environment.CurrentDirectory;
+                startInfo.FileName = Application.ExecutablePath;
+                startInfo.Verb = "runas";
+                try
+                {
+                    Process p = Process.Start(startInfo);
+                }
+                catch (System.ComponentModel.Win32Exception ex)
+                {
+                    return;
+                }
+
+            }
+        }
+    }
+}
